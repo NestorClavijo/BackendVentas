fe3fd21286bf9ccef6862f831d2970325794e9ad
NestorClavijo <nestor2211629@correo.uis.edu.co>
2024-11-25 12:46:50 -0500

    actualizada la URL del endpoint para obtener el informe en el dashboard del gerente

--- informes/src/pages/GerenteDashboard.js
+++ informes/src/pages/GerenteDashboard.js
@@ -17,7 +17,7 @@ function Gerencia() {
 
     try {
       // Llamada al endpoint para obtener el informe
-      const response = await axios.get('http://backendventas-production.up.railway.app/api/venta/informe');
+      const response = await axios.get('https://backendventas-production.up.railway.app/api/venta/informe');
       setInformeData(response.data); // Guardar los datos en el estado
       setShowInforme(true); // Mostrar el informe
     } catch (error) {
